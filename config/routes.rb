ecb0a9ca6b718c1d2de27805482a42b56721d23b
konnkonn <lily.of.zero@gmail.com>
2014-02-10 19:58:26 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+#Example::Application.routes.draw do
 Example::Application.routes.draw do
+  resources :projects
+  # root 'projects#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
